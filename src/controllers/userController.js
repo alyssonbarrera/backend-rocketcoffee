5c2d4ae2d4085d1caedb7fb0c6c948c86fe40965
Alysson Barrera <alyssonbarrera@outlook.com>
2022-06-16 23:40:31 -0300

    :seedling: database link change and improvements

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,8 +22,6 @@ const createUser = async (req, res) => {
     req.body.password = hashedPassword;
     
     try {
-        console.log(req.body)
-        console.log(req.file)
         const image = await cloudinary.uploader.upload(req.file.path);
         const imageURL = image.url;
 
